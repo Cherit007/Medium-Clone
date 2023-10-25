c2ddfe7d7654d7cd1c77171d48a51dce44fdd21c
dhanush77777 <saidhanushm@gmail.com>
2023-10-25 21:37:17 +0530

    few additions in ASKAI

--- AskAI/main.py
+++ AskAI/main.py
@@ -21,7 +21,22 @@ def askai():
         presence_penalty=0.17)
     res=response.choices[0].text
     res=res.split("#")
-    return jsonify({"result":res})
+
+    prompt2 = "I will give you the array of sub points of the article topic , please write 1-2 lines of description for each point in the array which i will provide and after each point add '$' symbol , the title is {} and the array of topics are {}.Dont include the array points in the response and the number of points should match number of descriptions you have provided".format(text,res)
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt2,
+        temperature=0.314,
+        max_tokens=256,
+        top_p=0.54,
+        frequency_penalty=0.44,
+        presence_penalty=0.17)
+    resp=response.choices[0].text
+    resp=resp.split("$")
+    a=[]
+    for i in range(min(len(res),len(resp))):
+        a.append({"point":res[i],"content":resp[i]})
+    return a
                
 
 if __name__ == "__main__":
