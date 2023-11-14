053bb07e4f0c714f8edb4a9b3eee26b57bf0a515
Cherit <cherit@Cherits-MacBook-Air.local>
2023-11-14 12:40:09 +0530

    latest chages

--- lib/redirect-user.ts
+++ lib/redirect-user.ts
@@ -1,7 +1,8 @@
+import { redirect } from "next/navigation";
 import { currentProfile } from "./current-profile";
 
 export const redirectUser = async () => {
   const user = await currentProfile();
-  if (user?.signUpStatus === "register") window.location.href = "/register";
-  else if (user?.signUpStatus === "welcome") window.location.href = "/welcome";
+  if (user?.signUpStatus === "register") redirect("/register");
+  else if (user?.signUpStatus === "welcome") redirect("/welcome");
 };
